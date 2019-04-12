dea126dcd76610ba87f3aee8c9d03374fb40030c
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-12 14:04:42 -0500

    Ready to build app

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.use(morgan('tiny'));
 app.use(express.static(`${__dirname}/public`));
@@ -14,7 +16,7 @@ app.use('/js', express.static(`${__dirname}/node_modules/bootstrap/dist/js`));
 app.use('/js', express.static(`${__dirname}/node_modules/jquery/dist`));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
