dcc216e4230da28b4db29183f10aee09929d7b78
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-12 14:58:31 -0500

    Set up main page and added routes for books and single

--- app.js
+++ app.js
@@ -6,17 +6,34 @@ const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
 app.use(morgan('tiny'));
 app.use(express.static(`${__dirname}/public`));
 app.use('/css', express.static(`${__dirname}/node_modules/bootstrap/dist/css`));
 app.use('/js', express.static(`${__dirname}/node_modules/bootstrap/dist/js`));
 app.use('/js', express.static(`${__dirname}/node_modules/jquery/dist`));
 
+bookRouter.route('/')
+  .get((reg, res) => {
+    res.send('Hello books');
+  });
+
+bookRouter.route('/single')
+  .get((reg, res) => {
+    res.send('Hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'Library' });
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
