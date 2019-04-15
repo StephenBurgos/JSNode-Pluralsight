1963aa5e28d9fb99a4b388309ae176242ff2de0c
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-15 10:38:33 -0500

    Set up sql connecting

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const config = {
+  user: 'Library',
+  password: 'psL1brary',
+  server: 'pslibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'PSLibrary',
+
+  options: {
+    encrypt: true, // Use this if you're on Windows Azure
+  },
+};
+
+sql.connect(config).catch(err => debug(err));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,43 +1,41 @@
 const express = require('express');
+const sql = require('mssql');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [{
-    title: 'The Time Machine',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false,
-  },
-  {
-    title: 'The Dark World',
-    genre: 'Fantasy',
-    author: 'Henry Kuttner',
-    read: false,
-  }];
-
   bookRouter.route('/')
     .get((req, res) => {
-      res.render(
-        'bookListView',
-        {
-          nav,
-          title: 'Library',
-          books,
-        },
-      );
+      (async function query() {
+        const request = new sql.Request();
+        const { recordset } = await request.query('select * from books');
+        res.render(
+          'bookListView',
+          {
+            nav,
+            title: 'Library',
+            books: recordset,
+          },
+        );
+      }());
     });
 
   bookRouter.route('/:id')
     .get((req, res) => {
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: books[req.params.id],
-        },
-      );
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request.input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        res.render(
+          'bookView',
+          {
+            nav,
+            title: 'Library',
+            book: recordset[0],
+          },
+        );
+      }());
       res.send('Hello single book');
     });
   return bookRouter;
